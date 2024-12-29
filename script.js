6e8ab8b3740c6047efd2290b00ddf54b8c4c955a
Shriram-Vibhute <shriram.vibhute08@gmail.com>
2024-12-29 12:54:48 +0530

    added functions

--- script.js
+++ script.js
@@ -3,4 +3,12 @@ function addTwoNumbers(a, b) {
     return a + b;
 }
 
-console.log(addTwoNumbers(5, 3));
+console.log(addTwoNumbers(5, 3)); 
+
+function multiplyTwoNumbers(a, b) {
+    return a * b;
+}
+
+function divideTwoNumbers(a, b) {
+    return a / b;
+}
