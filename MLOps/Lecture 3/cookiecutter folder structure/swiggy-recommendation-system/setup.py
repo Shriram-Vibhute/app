8e610e9c6741b92af157dda31ae33377fb47ac37
Shriram-Vibhute <shriram.vibhute08@gmail.com>
2025-01-03 14:23:43 +0530

    Day 3 - Add initial project structure for Swiggy recommendation system

---
+++ MLOps/Lecture 3/cookiecutter folder structure/swiggy-recommendation-system/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project will help for recommending to uses the top 10 food items based on there recent interest and interactions with application',
+    author='Team Brute Force',
+    license='',
+)
